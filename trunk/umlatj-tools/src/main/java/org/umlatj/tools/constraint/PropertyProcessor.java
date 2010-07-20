93280ede5bd704f34b2ed65f3e545f900794bbf8
laurent.d.legrand@gmail.com <laurent.d.legrand@gmail.com@600dadb2-8fb5-6c78-8a5c-82bd0601b3a6>
2010-07-20 20:38:07 +0000

    dummy implementation, for test

--- trunk/umlatj-tools/src/main/java/org/umlatj/tools/constraint/PropertyProcessor.java
+++ trunk/umlatj-tools/src/main/java/org/umlatj/tools/constraint/PropertyProcessor.java
@@ -4,7 +4,12 @@ import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic.Kind;
 
 import org.umlatj.kernel.Property;
 
@@ -12,14 +17,19 @@ import org.umlatj.kernel.Property;
  * Verify usage of {@link Property}
  * 
  * @author Laurent Legrand
- *
+ * 
  */
+@SupportedAnnotationTypes("org.umlatj.kernel.Property")
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class PropertyProcessor extends AbstractProcessor {
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations,
 			RoundEnvironment roundEnv) {
-		// TODO Auto-generated method stub
+		
+		for (Element element: roundEnv.getElementsAnnotatedWith(Property.class)) {
+			this.processingEnv.getMessager().printMessage(Kind.WARNING, "property found", element);
+		}
 		return false;
 	}
 
